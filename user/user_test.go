cccca5f14718257d3e97599acb4eb34292820f10
christianotieno <christian.otieno@saltpay.co>
2023-06-12 20:35:44 +0100

    test: add benchmark testing

--- user/user_test.go
+++ user/user_test.go
@@ -5,12 +5,137 @@ import (
 	"gopkg.in/mgo.v2/bson"
 	"os"
 	"reflect"
+	"strconv"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
 	m.Run()
 	os.Remove(dbPath)
+}
+
+func cleanDb(b *testing.B) {
+	os.Remove(dbPath)
+	u := &User{
+		ID:   bson.NewObjectId(),
+		Name: "John",
+		Role: "Tester",
+	}
+	err := u.Save()
+	if err != nil {
+		b.Fatalf("Error saving a user: %s", err)
+	}
+	b.ResetTimer()
+}
+
+func BenchmarkCreate(b *testing.B) {
+	cleanDb(b)
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		u := &User{
+			ID:   bson.NewObjectId(),
+			Name: "John_" + strconv.Itoa(i),
+			Role: "Tester",
+		}
+		b.StartTimer()
+		err := u.Save()
+		if err != nil {
+			b.Fatalf("Error saving a user: %s", err)
+		}
+	}
+}
+
+func BenchmarkRead(b *testing.B) {
+	cleanDb(b)
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		u := &User{
+			ID:   bson.NewObjectId(),
+			Name: "John_" + strconv.Itoa(i),
+			Role: "Tester",
+		}
+		err := u.Save()
+		if err != nil {
+			b.Fatalf("Error saving a user: %s", err)
+		}
+		b.StartTimer()
+		_, err = One(u.ID)
+		if err != nil {
+			b.Fatalf("Error retrieving a user: %s", err)
+		}
+	}
+}
+
+func BenchmarkUpdate(b *testing.B) {
+	cleanDb(b)
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		u := &User{
+			ID:   bson.NewObjectId(),
+			Name: "John_" + strconv.Itoa(i),
+			Role: "Tester",
+		}
+		err := u.Save()
+		if err != nil {
+			b.Fatalf("Error saving a user: %s", err)
+		}
+		b.StartTimer()
+		u.Role = "Developer"
+		err = u.Save()
+		if err != nil {
+			b.Fatalf("Error saving a user: %s", err)
+		}
+	}
+}
+
+func BenchmarkDelete(b *testing.B) {
+	cleanDb(b)
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		u := &User{
+			ID:   bson.NewObjectId(),
+			Name: "John_" + strconv.Itoa(i),
+			Role: "Tester",
+		}
+		err := u.Save()
+		if err != nil {
+			b.Fatalf("Error saving a user: %s", err)
+		}
+		b.StartTimer()
+		err = Delete(u.ID)
+		if err != nil {
+			b.Fatalf("Error deleting a user: %s", err)
+		}
+	}
+}
+
+func BenchmarkCRUD(b *testing.B) {
+	os.Remove(dbPath)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		u := &User{
+			ID:   bson.NewObjectId(),
+			Name: "John",
+			Role: "Tester",
+		}
+		err := u.Save()
+		if err != nil {
+			b.Fatalf("Error saving a user: %s", err)
+		}
+		_, err = One(u.ID)
+		if err != nil {
+			b.Fatalf("Error retrieving a user: %s", err)
+		}
+		u.Role = "Developer"
+		err = u.Save()
+		if err != nil {
+			b.Fatalf("Error updating a user: %s", err)
+		}
+		err = Delete(u.ID)
+		if err != nil {
+			b.Fatalf("Error deleting a user: %s", err)
+		}
+	}
 }
 
 func TestCRUD(t *testing.T) {
